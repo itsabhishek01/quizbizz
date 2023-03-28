3bfe41d82a78ccccfb5abe8e8b369d9f41339c26
Abhishek <abhibhardwaj636@gmail.com>
2023-03-28 22:34:36 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Quiz from './Components/Quiz'
-import JoinQuiz from './Components/JoinQuiz';
 import { BrowserRouter, Routes } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 
